c81683afa8e81bf3825d5ff201f1d24881cf0b65
wangchen1ren <wangchen1ren@gmail.com>
2013-11-14 12:23:48 +0800

    update tfs-checkout

--- tfs-checkout.sh
+++ tfs-checkout.sh
@@ -3,23 +3,52 @@
 dir=`dirname $0`
 dir=`cd $dir; pwd`
 
-collection=$1
-project=$2
-user=$3
+# GET ARGS
+while getopts c:p:d:u: opt; do
+    case $opt in
+        c) COLLECTION=$OPTARG;;
+        d) DIR=$OPTARG;;
+        u) USERPASS=$OPTARG;;
+    esac
+done
 
-echo $collection, $project $user
+# SET TF PATH
+TF_IN_PATH=`which tf 2>/dev/null`
+if [ -f ${TF_IN_PATH} ]; then
+    TF_DIR=`dirname "$TF_IN_PATH"`
+fi
+TF_HOME=${TF_HOME:-$TF_DIR}
+TF_BIN="$TF_HOME/tf"
+if [ ! -f $TF_BIN ]; then
+    echo "ERROR: tf not found!"
+    exit 4
+fi
 
-workdir="$dir/$project"
+# USAGE
+function usage() {
+    echo "Usage: `basename $0` -c collection -d dir -u user,pass"
+    exit 1
+}
+
+if [ "X$DIR" = 'X' ] || [ "X$COLLECTION" = 'X' ] || [ "X$USERPASS" = 'X' ] ; then
+    usage
+fi
+
+# MAIN
+name=`basename $DIR`
+workspace="TMP_$name"
+workdir="$dir/$name"
 mkdir -p $workdir
 
-tf workspace -new $project -collection:$collection -login:$user
+$TF_BIN workspace -new $workspace -collection:$COLLECTION -login:$USERPASS
 
-tf workfold -map -collection:$collection -workspace:$project -login:$user $/$project $workdir
+echo "$TF_BIN workfold -map -collection:$COLLECTION -workspace:$workspace -login:$USERPASS $DIR $workdir"
+$TF_BIN workfold -map -collection:$COLLECTION -workspace:$workspace -login:$USERPASS $DIR $workdir
 
 cd $workdir
-tf get -recursive -login:$user
+$TF_BIN get -recursive -login:$USERPASS
 cd $dir
 
-tf workfold -unmap -collection:$collection -workspace:$project -login:$user $workdir
+$TF_BIN workfold -unmap -collection:$COLLECTION -workspace:$workspace -login:$USERPASS $workdir
 
-tf workspace -delete $project -collection:$collection -login:$user
+$TF_BIN workspace -delete $workspace -collection:$COLLECTION -login:$USERPASS
